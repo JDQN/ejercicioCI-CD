971829e189c2a8acafcfc3a8263f187464c27b10
Juan David Quimbayo Nuñez <juan.quimbayo@sofka.com.co>
2023-10-03 17:56:29 -0500

    Se crea la clase user con todos los parametros requeridos

---
+++ src/main/java/org/example/entities/User.java
@@ -0,0 +1,25 @@
+package org.example.entities;
+
+public class User {
+
+	public String name;
+
+	public User(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String newName) {
+		this.name = newName;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"name='" + name + '\'' +
+				'}';
+	}
+}
